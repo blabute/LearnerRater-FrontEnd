2014dba4e84ded4277786ed6568c9b7bd649e5e3
Brysen LaBute <blabute@appliedsystems.com>
2017-06-19 10:55:23 -0400

    add redux-form reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,12 @@
 import { combineReducers } from 'redux';
 import {routerReducer} from 'react-router-redux';
 import courses from './courseReducer';
-
-
+import { reducer as reduxFormReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
-  routing: routerReducer,courses
+  routing: routerReducer,
+  courses,
+  form: reduxFormReducer
 });
 
 export default rootReducer;
