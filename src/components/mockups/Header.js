2328c3e8ef86b0f09939a0ee609e99b0ce0c5055
Shayne <srogers@appliedsystems.com>
2017-06-20 11:08:53 -0400

    adding sass files and index.htm and Header.js

---
+++ src/components/mockups/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+
+    <div className="header">
+
+      <h1>LearnerRater</h1>
+      <h2>So you learned something. Now let us know what you think.</h2>
+
+    </div>
+
+  );
+};
+
+export default Header;
