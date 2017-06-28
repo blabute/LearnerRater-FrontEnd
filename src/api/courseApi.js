c909d4fc5118ad745e784ffd00a41837801aac89
Mostafa Monwar <mmonwar@appliedsystems.com>
2017-06-28 11:54:52 -0400

    updated save issue

--- src/api/courseApi.js
+++ src/api/courseApi.js
@@ -3,19 +3,35 @@ import $ from 'jquery';
 
 class CourseApi {
 
-static saveCourse(category, course) {
+  static saveCourse(category, course) {
+      const {Category="",Title="", Author="", Description="", Website="", URL="", Username="", Rating="", Comment=""} = course;
+      const dataJson = {
+            "Author":Author,
+            "Category":Category,
+            "Description":Description,
+            "Reviews":[{
+                "Comment":Comment,
+                "Rating":Rating,
+                "Username":Username
+              }
+            ],
+            "Title":Title,
+            "URL":URL,
+            "Website":Website
+          };
 
-    return $.ajax({
-      type: "POST",
-      contentType: "application/json",
-      url: `${constants.REST_SERVICE_URL}AddResource?category=${category}`,
-      data:  JSON.stringify(course),
-    })
-    .then((id) => {
-      return Object.assign({}, course, { ID: id });
-    });
+      return $.ajax({
+        type: "POST",
+        contentType: "application/json",
+        url: `${constants.REST_SERVICE_URL}AddResource?category=${Category}`,
+        data:  JSON.stringify(dataJson),
+      })
+      .then((id) => {
+        return Object.assign({}, course, { ID: id });
+      });
+    }
+
   }
 
-}
 
 export default CourseApi;
