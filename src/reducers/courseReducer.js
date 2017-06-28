6e5e47060e83fd0a6b2eb32342eb66157c71fd74
Mostafa Monwar <mmonwar@appliedsystems.com>
2017-06-28 14:36:36 -0400

    updated reducer to display courses after saving

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,12 +1,12 @@
-import * as types from '../actions/actionTypes';
-export default function courseReducer(state=[], action){
-  switch(action.type) {
-    case types.CREATE_COURSE_SUCCESS:
-      return [...state,
-        Object.assign({}, action.course)
-      ];
-    default:
-      return state;
+//import * as types from '../actions/actionTypes';
+//export default function courseReducer(state=[], action){
+  //switch(action.type) {
+    //case types.CREATE_COURSE_SUCCESS:
+      //return [...state,
+        //Object.assign({}, action.course)
+      //];
+    //default:
+      //return state;
 
-  }
-}
+  //}
+//}
